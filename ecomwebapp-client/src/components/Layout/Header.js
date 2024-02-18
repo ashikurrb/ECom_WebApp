5155e7a412c95ae4cf9d2b1b1d215acfa95f86ab
Ashikur Rahman Bhuiyan <ashikurrb@outlook.com>
2024-02-19 00:13:24 +0600

    Context API Added

--- ecomwebapp-client/src/components/Layout/Header.js
+++ ecomwebapp-client/src/components/Layout/Header.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import { NavLink, Link } from 'react-router-dom'
-import {GiShoppingCart } from 'react-icons/gi'
+import { GiShoppingCart } from 'react-icons/gi'
+import { useAuth } from '../context/auth';
+import {toast} from 'react-hot-toast';
 
 const Header = () => {
+    const [auth, setAuth] = useAuth();
+    const handleLogout = () => {
+        setAuth({
+            ...auth, user: null, token: ''
+        })
+        localStorage.removeItem('auth');
+        toast.success('Logout Successfully');
+    }
     return (
         <>
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
@@ -11,7 +21,7 @@ const Header = () => {
                         <span className="navbar-toggler-icon" />
                     </button>
                     <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-                        <Link to="/" className="navbar-brand"> <GiShoppingCart/> ECom_WebApp</Link>
+                        <Link to="/" className="navbar-brand"> <GiShoppingCart /> ECom_WebApp</Link>
                         <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                             <li className="nav-item">
                                 <NavLink to="/" className="nav-link">Home</NavLink>
@@ -19,12 +29,18 @@ const Header = () => {
                             <li className="nav-item">
                                 <NavLink to="/catagory" className="nav-link ">Catagory</NavLink>
                             </li>
-                            <li className="nav-item">
-                                <NavLink to="/register" className="nav-link">Register</NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink to="/login" className="nav-link">Log in</NavLink>
-                            </li>
+                            {!auth.user ? (<>
+                                <li className="nav-item">
+                                    <NavLink to="/register" className="nav-link">Register</NavLink>
+                                </li>
+                                <li className="nav-item">
+                                    <NavLink to="/login" className="nav-link">Log in</NavLink>
+                                </li>
+                            </>) : (<>
+                                <li className="nav-item">
+                                    <NavLink onClick={handleLogout} to="/login" className="nav-link">Logout</NavLink>
+                                </li>
+                            </>)}
                             <li className="nav-item">
                                 <NavLink to="/cart" className="nav-link">Cart (0)</NavLink>
                             </li>
--- ecomwebapp-client/src/pages/Auth/Login.js
+++ ecomwebapp-client/src/pages/Auth/Login.js
@@ -4,12 +4,14 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import toast from 'react-hot-toast';
 import '../../style/AuthStyle.css';
+import { useAuth } from '../../components/context/auth';
 
 const Login = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [auth, setAuth] = useAuth();
+
     const navigate = useNavigate();
-    
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -17,6 +19,12 @@ const Login = () => {
             const res = await axios.post(`${process.env.REACT_APP_API}/api/v1/auth/login`, { email, password });
             if (res && res.data.success) {
                 toast.success(res.data && res.data.message);
+                setAuth({
+                    ...auth,
+                    user: res.data.user,
+                    token: res.data.token
+                })
+                localStorage.setItem('auth', JSON.stringify(res.data))
                 navigate('/')
             } else {
                 toast.error(res.data.message)
@@ -28,18 +36,18 @@ const Login = () => {
     }
 
     return (
-        <Layout title={"Register"}>
+        <Layout title={"Log In"}>
             <div className="form-container ">
                 <form onSubmit={handleSubmit}>
                     <h4 className="title">Login Here</h4>
-                   
+
                     <div className="mb-3">
                         <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} className="form-control" id="exampleInputEmail" placeholder='Email' required />
                     </div>
                     <div className="mb-3">
                         <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="form-control" id="exampleInputPassword1" placeholder='Password' required />
                     </div>
-                   
+
                     <button type="submit" className="btn btn-primary">Log In</button>
                 </form>
             </div>
