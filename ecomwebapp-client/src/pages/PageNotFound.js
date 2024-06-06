d4ed0e4341d04a269076bcb0fb1e9ebebe5c5daa
Ashikur Rahman Bhuiyan <ashikurrb85@gmail.com>
2024-06-06 18:19:30 +0600

    Goback button added as a standalone component
    removed from pnf, added as a separate component

--- ecomwebapp-client/src/pages/PageNotFound.js
+++ ecomwebapp-client/src/pages/PageNotFound.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import Layout from '../components/Layout/Layout';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import GoBackButton from '../components/GoBackButton';
 
 const PageNotFound = () => {
-    const navigate = useNavigate();
-
-    const handleGoBack = () => {
-        navigate(-1);
-    };
     return (
         <Layout title={"Page Not Found"}>
             <div className="pnf">
                 <div className="pnf-title">404</div>
                 <h2 className="pnf-heading"> Ooops! Page Not Found</h2>
-                <button className='pnf-btn' onClick={handleGoBack}><b>Go Back</b></button>
+                <GoBackButton/>
                 <Link to="/" className="pnf-btn"><b>Go to Home</b></Link>
             </div>
         </Layout>
