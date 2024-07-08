4d8bf192d480d8e7338cf5a21e93baf9b50f013c
Ashikur Rahman Bhuiyan <ashikurrb85@gmail.com>
2024-07-08 18:23:40 +0600

    fix

--- ecomwebapp-client/src/components/Layout/Layout.js
+++ ecomwebapp-client/src/components/Layout/Layout.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import { Helmet } from 'react-helmet';
+import { Toaster } from 'react-hot-toast';
 
 
 const Layout = ({ children, title, description, keywords, author }) => {
