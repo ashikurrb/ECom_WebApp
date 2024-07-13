b6d063139f11746dd2b98946942dd7cac1906406
Ashikur Rahman Bhuiyan <ashikurrb85@gmail.com>
2024-07-13 16:06:02 +0600

    Update CartPage

--- ecomwebapp-client/src/pages/CartPage.js
+++ ecomwebapp-client/src/pages/CartPage.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Layout from '../components/Layout/Layout';
 import { useCart } from '../components/context/cart';
 import { useAuth } from '../components/context/auth';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import DropIn from "braintree-web-drop-in-react";
 import axios from 'axios';
 import toast from 'react-hot-toast';
@@ -14,9 +14,9 @@ const CartPage = () => {
     const [clientToken, setClientToken] = useState("");
     const [instance, setInstance] = useState("");
     const [loading, setLoading] = useState(false);
-
     const navigate = useNavigate();
 
+    //total pricing
     const totalPrice = () => {
         try {
             let total = 0;
@@ -30,21 +30,34 @@ const CartPage = () => {
         }
     };
 
+    // Increase adn Decrease Cart Item
+    const increaseCartItem = (item) => {
+        try {
+            setCart([...cart, item]);
+            localStorage.setItem('cart', JSON.stringify([...cart, item]));
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     const decreaseCartItem = (pid) => {
         try {
             let myCart = [...cart];
             let index = myCart.findIndex(item => item._id === pid);
-            myCart.splice(index, 1);
-            setCart(myCart);
-            localStorage.setItem('cart', JSON.stringify(myCart));
+            if (index !== -1) {
+                myCart.splice(index, 1);
+                setCart(myCart);
+                localStorage.setItem('cart', JSON.stringify(myCart));
+            }
         } catch (error) {
             console.log(error);
         }
     };
 
+    //remove cart item
     const removeCartItem = (pid) => {
         try {
-            let myCart = cart.filter(item => item._id !== pid); // Remove all items with the matching _id
+            let myCart = cart.filter(item => item._id !== pid);
             setCart(myCart);
             localStorage.setItem('cart', JSON.stringify(myCart));
         } catch (error) {
@@ -52,7 +65,17 @@ const CartPage = () => {
         }
     };
 
+    //show each product for one time with required quantity, avoid duplication in cart
+    const uniqueCartItems = Array.from(new Set(cart.map(item => item._id)))
+        .map(id => {
+            const item = cart.find(item => item._id === id);
+            return {
+                ...item,
+                count: cart.filter(cartItem => cartItem._id === id).length,
+            };
+        });
 
+    //get token for authentication
     const getToken = async () => {
         try {
             const { data } = await axios.get(`${process.env.REACT_APP_API}/api/v1/product/braintree/token`);
@@ -66,6 +89,7 @@ const CartPage = () => {
         getToken();
     }, [auth?.token]);
 
+    //payment handling 
     const handlePayment = async () => {
         try {
             setLoading(true);
@@ -85,20 +109,10 @@ const CartPage = () => {
         }
     };
 
-    const uniqueCartItems = Array.from(new Set(cart.map(item => item._id)))
-        .map(id => {
-            const item = cart.find(item => item._id === id);
-            return {
-                ...item,
-                count: cart.filter(cartItem => cartItem._id === id).length,
-
-            };
-        });
-
     return (
         <Layout>
             <div className="container">
-                <div className="row ">
+                <div className="row">
                     <div className="col-md-12">
                         <div className="row align-items-center bg-light my-2">
                             <div className="col-auto">
@@ -112,92 +126,60 @@ const CartPage = () => {
                         </div>
                         <h5 className='text-center my-4'>
                             {uniqueCartItems.length
-<<<<<<< Updated upstream
-                                ? `You have ${uniqueCartItems.length} items of ${cart.length} pieces in your cart. 
-                                ${auth?.token ? "" : "Please Log in to Checkout"}`
-                                : "Your Cart is Empty"}
-=======
                                 ? `You have ${cart.length} pieces of ${uniqueCartItems.length} items in your cart. ${auth?.token ? "" : "Please Log in to Checkout"}`
                                 : <>Your Cart is Empty. Visit {<Link to="/">Home</Link>} to order.</>}
->>>>>>> Stashed changes
                         </h5>
                     </div>
                 </div>
+
                 <div className="row">
                     <div className="col-md-8">
                         <div className="row mb-2">
-                            {uniqueCartItems.map(p => (
-                                <div className="row p-3 mb-2 card flex-row" style={{ width: '540rem' }} key={p._id}>
-                                    <div className="col-md-4">
-                                        <img src={`${process.env.REACT_APP_API}/api/v1/product/product-photo/${p._id}`} className="imgFit card-img-top" alt={p.name} width={"30px"} height={"100px"} />
-                                    </div>
-                                    <div className="col-md-8">
-                                        <h5 className="card-title">{p.name}</h5>
-                                        <h5>TK. {p.price}</h5>
-                                        <p className="card-text">{p.description.substring(0, 100)}...</p>
-                                        <div className='d-flex flex-wrap'>
-                                            <div className='me-auto'>
-                                                <button className='btn btn-danger' onClick={() => decreaseCartItem(p._id)}>-</button>
-                                                <span className='mx-1'><b>{p.count}</b></span>
-                                                <button className='btn btn-secondary m-1 '
-                                                    onClick={() => {
-                                                        setCart([...cart, p])
-                                                        localStorage.setItem('cart', JSON.stringify([...cart, p]))
-                                                    }}>
-                                                    +</button>
-                                            </div>
-                                            <div className='ms-auto'>
-                                                <button className='btn btn-danger' onClick={() => removeCartItem(p._id)}><i class="fa-solid fa-trash-can"></i></button>
-                                            </div>
-
-                                        </div>
-                                    </div>
-                                </div>
-                            ))}
-=======
                             <div className="table-container">
-                            <table className="table">
-                                <thead>
-                                    <tr>
-                                        <th>#</th>
-                                        <th>Photo</th>
-                                        <th>Name</th>
-                                        <th>Price</th>
-                                        <th>Qty</th>
-                                        <th>Remove</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {
-                                        uniqueCartItems.map((p, i) => (
-                                            <tr key={p._id}>
-                                                <td>{i + 1}</td>
-                                                <td>
-                                                    <Link to={`/product/${p.slug}`}>
-                                                        <img src={`${process.env.REACT_APP_API}/api/v1/product/product-photo/${p._id}`} className="imgFit img-fluid" alt={p.name} width={"50px"} height={"100px"} />
-                                                    </Link>
-                                                </td>
-                                                <td>{p.name}</td>
-                                                <td>{p.price}</td>
-                                                <td>
-                                               <div className="d-flex">
-                                               <button className='btn btn-danger' onClick={() => decreaseCartItem(p._id)}>-</button>
-                                                    <span className='mx-2 border border-warning p-2 rounded'><b>{p.count}</b></span>
-                                                    <button className='btn btn-secondary' onClick={() => increaseCartItem(p)}>+</button>
-                                               </div>
-                                                </td>
-                                                <td>
-                                                    <button className='btn btn-danger' onClick={() => removeCartItem(p._id)}><i className="fa-solid fa-trash-can"></i></button>
+                                <table className="table">
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Photo</th>
+                                            <th>Name</th>
+                                            <th>Price</th>
+                                            <th>Qty</th>
+                                            <th>Total</th>
+                                            <th>Remove</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {
+                                            uniqueCartItems.map((p, i) => (
+                                                <tr>
+                                                    <td>{i + 1}</td>
+                                                    <td>
+                                                        <Link to={`/product/${p.slug}`}>
+                                                            <img src={`${process.env.REACT_APP_API}/api/v1/product/product-photo/${p._id}`} className="imgFit img-fluid" alt={p.name} width={"50px"} height={"100px"} />
+                                                        </Link>
+                                                    </td>
+                                                    <td>{p.name}</td>
+                                                    <td>{p.price}</td>
+                                                    <td>
+                                                        <div className="d-flex">
+                                                            <button className='btn btn-danger' onClick={() => decreaseCartItem(p._id)}>-</button>
+                                                            <span className='mx-2 border border-warning p-2 rounded'><b>{p.count}</b></span>
+                                                            <button className='btn btn-secondary' onClick={() => increaseCartItem(p)}>+</button>
+                                                        </div>
+                                                    </td>
+                                                    <td>
+                                                       $ {p.price * p.count}
+                                                    </td>
+                                                    <td>
+                                                        <button className='btn btn-danger' onClick={() => removeCartItem(p._id)}><i className="fa-solid fa-trash-can"></i></button>
+                                                    </td>
+                                                </tr>
+                                            ))
+                                        }
 
-                                                </td>
-                                            </tr>
-                                        ))
-                                    }
-
-                                </tbody>
-                            </table>
+                                    </tbody>
+                                </table>
                             </div>
->>>>>>> Stashed changes
                         </div>
                     </div>
                     <div className="col-md-4">
@@ -208,20 +190,16 @@ const CartPage = () => {
                             <h6>Total Item: {uniqueCartItems.length}</h6>
                             <h6>Total Quantity: {cart.length}</h6>
                             {auth?.user?.address ? (
-                                <>
-                                    <div className="mb-3">
-                                        <p className='fw-bold'>Current Address: {auth?.user?.address}</p>
-                                        <button className='btn btn-warning' onClick={() => navigate("/dashboard/user/profile")}>Update Address</button>
-                                    </div>
-                                </>
+                                <div className="mb-3">
+                                    <p className='fw-bold'>Current Address: {auth?.user?.address}</p>
+                                    <button className='btn btn-warning' onClick={() => navigate("/dashboard/user/profile")}>Update Address</button>
+                                </div>
                             ) : (
                                 <div className="mb-3">
                                     {auth?.token ? (
                                         <button className='btn btn-outline-warning' onClick={() => navigate("/dashboard/user/profile")}>Update Address</button>
                                     ) : (
-                                        <button className='btn btn-warning' onClick={() => navigate("/login", {
-                                            state: "/cart"
-                                        })}>Please Login to Checkout</button>
+                                        <button className='btn btn-warning' onClick={() => navigate("/login", { state: "/cart" })}>Please Login to Checkout</button>
                                     )}
                                 </div>
                             )}
@@ -232,9 +210,7 @@ const CartPage = () => {
                                         <DropIn
                                             options={{
                                                 authorization: clientToken,
-                                                paypal: {
-                                                    flow: "vault",
-                                                },
+                                                paypal: { flow: "vault" },
                                             }}
                                             onInstance={(instance) => setInstance(instance)}
                                         />
